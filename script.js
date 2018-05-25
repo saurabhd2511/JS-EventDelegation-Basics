4bf00ab54ed77b5bb8bce77a1e9e8df732229200
Saurabh Deshpande <saurabhd2511@gmail.com>
2018-05-25 02:03:51 -0400

    Finished intro to browser events

--- script.js
+++ script.js
@@ -39,3 +39,55 @@ field.onclick = function(event) {
   ball.style.left = ballCoords.left + 'px';
   ball.style.top = ballCoords.top + 'px';
 }
+
+let menuElem = document.getElementById('sweeties');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};
+
+
+let panes = document.querySelectorAll('.pane');
+
+for(let pane of panes) {
+  pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+  // button becomes the first child of pane
+  pane.firstChild.onclick = () => pane.remove();
+}
+
+
+//Script for carousal task
+
+/* label the images, just for convenience, to visually track them */
+let i = 1;
+for(let li of carousel.querySelectorAll('li')) {
+  li.style.position = 'relative';
+  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+  i++;
+}
+
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
