76f56f744307b646c37fd8a98a4db8b0f595092c
Saurabh Deshpande <saurabhd2511@gmail.com>
2018-06-02 22:20:54 -0400

    Browser actions

--- script.js
+++ script.js
@@ -168,3 +168,22 @@ function sortGrid(colNum, type) {
 
   tbody.append(...rowsArray);
 }
+
+//
+//Chapter 4
+
+//Task1:Picture Gallery
+
+thumbs.onclick = function(event) {
+      let thumbnail = event.target.closest('a');
+
+      if (!thumbnail) return;
+      showThumbnail(thumbnail.href, thumbnail.title);
+      event.preventDefault();
+    }
+
+    function showThumbnail(href, title) {
+      largeImg.src = href;
+      largeImg.alt = title;
+    }
+
